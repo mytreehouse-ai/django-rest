26a7127b45c12e27d8bb20f524b8adb219d37e5c
casulit <chrisgelosulit@gmail.com>
2024-02-26 00:36:13 +0800

    fix(scraper_api): Include last page in scraping job range

--- scraper_api/tasks.py
+++ scraper_api/tasks.py
@@ -26,7 +26,7 @@ def scraperapi_process_scrapy_web():
     scrapy_webs = ScrapyJobService.get_all_scrapy_web()
 
     for scrapy_web in scrapy_webs:
-        for i in range(1, scrapy_web.page_number):
+        for i in range(1, scrapy_web.page_number + 1):
             payload = {
                 "apiKey": os.environ.get("SCRAPER_API_KEY"),
                 "url": f"{scrapy_web.web_url}/?page={i}",
