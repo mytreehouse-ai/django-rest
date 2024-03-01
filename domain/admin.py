f5db6bca21271464370052ae8bc2eff94468f6aa
casulit <chrisgelosulit@gmail.com>
2024-03-01 13:35:52 +0800

    feat(admin): Register CityModel with Django admin and configure display options

--- domain/admin.py
+++ domain/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.city_model import CityModel
+
+
+@admin.register(CityModel)
+class CityModelModel(admin.ModelAdmin):
+    list_display = (
+        "id", "name", "created_at", "updated_at"
+    )
+    search_fields = ("name",)
