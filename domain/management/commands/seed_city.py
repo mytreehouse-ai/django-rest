6c630d1e2bae96d3c8c0c14c7af1248f321a80d6
casulit <chrisgelosulit@gmail.com>
2024-03-01 13:26:05 +0800

    feat(seed_city): Capitalize city names when seeding from CSV

--- domain/management/commands/seed_city.py
+++ domain/management/commands/seed_city.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
             reader = csv.reader(csvfile)
             for row in reader:
                 _, created = CityModel.objects.get_or_create(
-                    name=row[2]
+                    name=row[2].title()
                 )
                 if created:
                     self.stdout.write(self.style.SUCCESS(
