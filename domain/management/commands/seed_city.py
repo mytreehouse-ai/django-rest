4008b505cd0f6934b440392215c5b1c0db61e319
casulit <chrisgelosulit@gmail.com>
2024-03-01 18:36:06 +0800

    refactor: Standardize ñ character replacement across modules

--- domain/management/commands/seed_city.py
+++ domain/management/commands/seed_city.py
@@ -1,3 +1,4 @@
+import re
 import csv
 from django.core.management.base import BaseCommand
 from domain.models.city_model import CityModel
@@ -10,12 +11,14 @@ class Command(BaseCommand):
         with open('domain/csv/refcitymun.csv', mode='r', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
+                city = re.sub(r'[\ufffd\u00f1]', 'ñ', row[2])
+
                 _, created = CityModel.objects.get_or_create(
-                    name=row[2].capitalize()
+                    city.capitalize()
                 )
                 if created:
                     self.stdout.write(self.style.SUCCESS(
-                        f'Successfully added city {row[2]}'))
+                        f'Successfully added city {city}'))
                 else:
                     self.stdout.write(self.style.WARNING(
-                        f'City {row[2]} already exists'))
+                        f'City {city} already exists'))
