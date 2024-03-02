402bf15f8d8eac4d7ab3273a8b27fecf80b8a944
casulit <chrisgelosulit@gmail.com>
2024-03-02 23:10:58 +0800

    refactor(scraper_api): Replace print with logger in webhook

--- scraper_api/views/update_property_webhook.py
+++ scraper_api/views/update_property_webhook.py
@@ -121,7 +121,8 @@ class UpdatePropertyWebhookAPIView(UpdateAPIView):
                     listing_url=listing_url
                 )
             except PropertyListingModel.DoesNotExist:
-                print(f"No property listing found for URL: {listing_url}")
+                logger.error(
+                    f"No property listing found for URL: {listing_url}")
                 return
 
             property_listing.listing_title = title
@@ -161,7 +162,14 @@ class UpdatePropertyWebhookAPIView(UpdateAPIView):
                     ]
                 )
 
-            print(
+            try:
+                scrapy_job = ScrapyJobModel.objects.get(domain=listing_url)
+                scrapy_job.is_single_page_processed = True
+                scrapy_job.save(update_fields=["is_single_page_processed"])
+            except ScrapyJobModel.DoesNotExist:
+                logger.error(f"Scrapy job not found: {listing_url}")
+
+            logger.info(
                 f"Property listing found: {property_listing.listing_url}"
             )
         return Response(
