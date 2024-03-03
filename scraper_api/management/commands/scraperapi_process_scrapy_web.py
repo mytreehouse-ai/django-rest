e4839b36f5bb7a883c35778c9f89e2b24f72ab61
casulit <chrisgelosulit@gmail.com>
2024-03-03 15:02:55 +0800

    refactor(scraper_api): Remove unused is_single_page_processed

--- scraper_api/management/commands/scraperapi_process_scrapy_web.py
+++ scraper_api/management/commands/scraperapi_process_scrapy_web.py
@@ -69,7 +69,6 @@ class Command(BaseCommand):
                             "status_url": response_json.get("status_url", None),
                             "supposed_to_run_at": response_json.get("supposedToRunAt", None),
                             "is_multi_page_processed": False,
-                            "is_single_page_processed": False
                         }
                     )
                 except ValueError:
