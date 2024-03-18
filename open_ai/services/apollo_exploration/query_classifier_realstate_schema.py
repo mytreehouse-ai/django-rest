67de064635720c9c5618a22fe1442474a1d9a769
casulit <chrisgelosulit@gmail.com>
2024-03-18 18:18:33 +0800

    docs: Update real estate schema description

--- open_ai/services/apollo_exploration/query_classifier_realstate_schema.py
+++ open_ai/services/apollo_exploration/query_classifier_realstate_schema.py
@@ -11,7 +11,7 @@ query_classifier_realstate_schema = [
     ),
     ResponseSchema(
         name="for_vector_search",
-        description="For the real estate query type, this schema is responsible for generating a comprehensive query that incorporates all relevant extracted information for input into our vector search engine",
+        description="For the real estate query type, this schema is responsible for generating a comprehensive query that incorporates all relevant extracted information for input into our vector search engine. Keep this a string format example: 'property_type: condominium budget_price: 10,000,000.00 - 20,000,000.00 location: taguig features: with swimming pool etc... include important information to improve the similarity search.'",
         type="string"
     ),
 ]
