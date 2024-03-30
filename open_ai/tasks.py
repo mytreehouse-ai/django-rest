f5f3410d7007a711c9e71a34cd1cc5ff2d58d1ee
casulit <chrisgelosulit@gmail.com>
2024-03-30 11:12:18 +0800

    refactor(tasks): Update query limit in reset markdown

--- open_ai/tasks.py
+++ open_ai/tasks.py
@@ -31,9 +31,7 @@ def update_available_cities_for_ai_context():
 def reset_markdown_description_for_property():
     property_listings = PropertyListingModel.objects.filter(
         is_active=True,
-        estate__markdown__isnull=False,
-        estate__ai_generated_description=True
-    ).order_by('-id')[:25]
+    ).order_by('-id')[:100]
 
     for property in property_listings:
         description_text = property.estate.metadata.get(
